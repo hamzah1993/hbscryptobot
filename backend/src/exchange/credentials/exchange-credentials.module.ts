e444b259bf5a735b250d2d8f19e0de3e54e9a77f
hamzah1993 <154465959+hamzah1993@users.noreply.github.com>
2026-08-06 02:23:55 +0500

    fix: resolve exchange credential module cycle

--- backend/src/exchange/credentials/exchange-credentials.module.ts
+++ backend/src/exchange/credentials/exchange-credentials.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { BinanceModule } from '../binance/binance.module';
 import { CredentialCryptoService } from './credential-crypto.service';
 import { ExchangeAccountManagementService } from './exchange-account-management.service';
@@ -6,7 +6,7 @@ import { ExchangeCredentialsController } from './exchange-credentials.controller
 import { ExchangeCredentialsService } from './exchange-credentials.service';
 
 @Module({
-  imports: [BinanceModule],
+  imports: [forwardRef(() => BinanceModule)],
   controllers: [ExchangeCredentialsController],
   providers: [
     CredentialCryptoService,
